175dfbb47c9bbc7706250493d3041cba9693a7e8
Sujit Warrier <sujit.warrier11@gmail.com>
2025-01-03 17:25:36 +0530

    add validation error handler

--- backend/src/run.py
+++ backend/src/run.py
@@ -1,8 +1,9 @@
 """Main configuration file for quart"""
 
 from quart import Quart, ResponseReturnValue  # type: ignore
-from quart_auth import AuthManager # type: ignore
-from quart_rate_limiter import  RateLimiter, RateLimitExceeded
+from quart_auth import AuthManager  # type: ignore
+from quart_rate_limiter import RateLimiter, RateLimitExceeded
+from quart_schema import RequestSchemaValidationError
 
 from backend.src.lib.api_error import APIError
 
@@ -21,10 +22,24 @@ async def handle_api_error(error: APIError) -> ResponseReturnValue:
 
 @app.errorhandler(RateLimitExceeded)
 async def handle_rate_limit_exceeded_error(
-    error: RateLimitExceeded
+    error: RateLimitExceeded,
 ) -> ResponseReturnValue:
     """Handle rate limit errors"""
     return {}, error.get_headers(), 429
+
+
+@app.errorhandler(RequestSchemaValidationError)
+async def handle_request_schema_validation_error(
+    error: RequestSchemaValidationError,
+) -> ResponseReturnValue:
+    """Handle request schema validation errors"""
+    return {
+        "errors": (
+            str(error.validation_error)
+            if isinstance(error.validation_error, TypeError)
+            else error.validtion_error.json()
+        )
+    }, 400
 
 
 @app.get("/health")
